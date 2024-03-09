a6c0db4047882526a4bc5d2a6a53a7c88dcfb0d6
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-09 18:00:58 +0600

    alhamdulillah module-35 finished

--- try-catch.js
+++ try-catch.js
@@ -1,4 +1,6 @@
 // 35-8 Error Handling: Try, Catch, Throw And Finally
+// 35-9 Module Summary And Interview Questions
+// 35-10 Frying The Concept Of JS Event Loop
 function checkAge() {
     const chkAge = document.getElementById('age');
     // console.log(chkAge);
@@ -35,3 +37,8 @@ function checkAge() {
     }
     console.log(11111);
 }
+
+//JS interview question:
+
+//https://www.jsv9000.app/
+//http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
